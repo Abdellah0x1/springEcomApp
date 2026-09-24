9912b43db4077ae7ea9789f47c974413613ed663
abdellah0x1 <abellah.elghennami@usmba.ac.ma>
2026-09-24 16:23:33 +0000

    Switching from ChannelInterceptor → ExecutorChannelInterceptor resolves the compilation error

--- src/main/java/com/ecommerce/project/Websockets/WebSocketAuthInterceptor.java
+++ src/main/java/com/ecommerce/project/Websockets/WebSocketAuthInterceptor.java
@@ -9,7 +9,7 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.ExecutorChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,7 +20,7 @@ import java.security.Principal;
 import java.util.List;
 
 @Component
-public class WebSocketAuthInterceptor implements ChannelInterceptor {
+public class WebSocketAuthInterceptor implements ExecutorChannelInterceptor {
 
     @Autowired
     private JwtUtils jwtUtils;
